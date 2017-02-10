efb1c08e939d5b9fb015eb18e83c9163db260fb5
AndreaValenziano <valenzianoa@gmail.com>
2017-02-10 10:32:05 +0100

    firstApp

---
+++ app/src/main/java/com/example/andreavalenziano/myfirstapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.andreavalenziano.myfirstapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by AndreaValenziano on 07/02/17.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
